0423bdab7ce35de3d3f020ed3b31643d921c97c4
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-02-19 19:11:47 -0500

    i've adeed the users in the frontend

---
+++ src/main/java/sl/contactos/controlador/ContactoControlador.java
@@ -0,0 +1,31 @@
+package sl.contactos.controlador;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import sl.contactos.modelo.Contacto;
+import sl.contactos.servicio.ContactoServicio;
+
+import java.util.List;
+
+@Controller
+public class ContactoControlador {
+    private static final Logger logger = LoggerFactory.getLogger(ContactoControlador.class);
+
+    @Autowired
+    private ContactoServicio contactoServicio;
+
+//    Notacion para procesar url
+    @GetMapping("/")
+    public String iniciar(ModelMap modelo){
+        List<Contacto> contactos = contactoServicio.mostrarContactos();
+        contactos.forEach((contacto) -> logger.info(contacto.toString()));
+//     Compartir datos desde el backend al frontend
+        modelo.put("contactos", contactos);
+        return "index";//index.html
+    }
+
+}
--- src/main/java/sl/contactos/modelo/Contacto.java
+++ src/main/java/sl/contactos/modelo/Contacto.java
@@ -6,12 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
+import java.util.Objects;
+
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode
-@ToString
 public class Contacto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +16,69 @@ public class Contacto {
     String nombre;
     String celular;
     String email;
+
+    public Contacto() {
+    }
+
+    public Contacto(Integer id, String nombre, String celular, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.celular = celular;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto = (Contacto) o;
+        return Objects.equals(id, contacto.id) && Objects.equals(nombre, contacto.nombre) && Objects.equals(celular, contacto.celular) && Objects.equals(email, contacto.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, celular, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Contacto{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", celular='" + celular + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
